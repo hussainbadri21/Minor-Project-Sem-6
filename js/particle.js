4fcb23716e08b770c61cda73a1b20dd9ae9cec68
MOHIT TIWARI <mohitt533@gmail.com>
2017-02-16 18:08:20 +0530

    UI enhancement and login form

--- js/particle.js
+++ js/particle.js
@@ -108,7 +108,7 @@ particlesJS("particles-js", {
   },
   "retina_detect": true
 });
-var count_particles, stats, update;
+/*var count_particles, stats, update;
 stats = new Stats;
 stats.setMode(0);
 stats.domElement.style.position = 'absolute';
@@ -124,4 +124,4 @@ update = function() {
   }
   requestAnimationFrame(update);
 };
-requestAnimationFrame(update);;
+requestAnimationFrame(update);;*/
